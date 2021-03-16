c46d80b8a0018a15074787f7114b2425d87230d6
Victor Gaugler <victor.gaugler@hotmail.fr>
2021-03-16 12:50:58 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,160 @@
+const searchInput = document.getElementById('searchInput');
+const results = document.getElementById('results');
+const randomMeal = document.getElementById('randomMeal');
+const banner = document.querySelector('.banner');
+const bannerBox = document.querySelector('.bannerBox');
+const searchBox = document.querySelector('.searchBox');
+const searchIcon = document.getElementById('searchIcon');
+
+let search = '';
+
+window.addEventListener("scroll", function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
+
+
+function toggleMenu(){
+var menuToggle = document.querySelector('.toggle');
+var menu = document.querySelector('.menu');
+menuToggle.classList.toggle('active');
+menu.classList.toggle('active');
+
+}
+
+
+
+
+searchInput.addEventListener('focus',function(event){
+    banner.style.backgroundImage = "url('banner2.jpg')";
+    banner.style.transform = 'scale(1.1)';
+    bannerBox.style.transform = "translateX(350px)";
+    event.preventDefault();
+    event.stopPropagation();
+   
+
+});
+
+searchIcon.addEventListener('click', function(){
+    bannerBox.style.transform = "translateX(-5px)";
+});
+
+searchInput.addEventListener('focusout', function(event){
+    banner.style.backgroundImage = "url('banner.jpg')";
+    banner.style.transform =  'scale(1)';
+    
+    event.preventDefault();
+    event.stopPropagation();
+  
+});
+
+var topOffset;
+
+$(window).scroll(function() {
+	if($(this).scrollTop() < 1) {
+	 topOffset = 55;
+	} else {
+	 topOffset = 50;
+  }
+});
+
+$('body a[href^="#"]').on('click', function(event) {
+    $('.active').removeClass('active');
+    $(this).addClass('active');
+
+  var target = $(this.getAttribute('href'));
+  if( target.length ) {
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top - topOffset
+      }, 1000);
+  }
+});
+
+const fetchSearch = async() => {
+    meals = await fetch(
+        `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
+        .then(res => res.json())
+        .then(res => res.meals);
+        console.log(meals);
+}
+
+const searchDisplay = async () => {
+    await fetchSearch();
+    if (meals == null){
+        results.innerHTML = '<span class="noResult">No results</span>';
+    }
+    
+   /* results.innerHTML = ( 
+
+        meals.map(meal => (
+            
+            `
+            <div class="searchContainer" id="boxResize-${meal.idMeal}">
+                
+                <img id="photo" src='${meal.strMealThumb}'/>
+
+                
+                <div class="infos">
+                <h2>${meal.strMeal}</h2>
+                <div>origin : ${meal.strArea}</div>
+                <button id="${meal.idMeal}"> En savoir plus </button>
+                </div>
+                
+                
+            </div>
+            `
+        )).join('')
+    ); */
+
+    for ( let i = 0; i<meals.length; i++){
+        
+        const newDiv = document.createElement('div');
+        
+        const container = `
+        
+        
+            
+            <img id="photo" src='${meals[i].strMealThumb}'/>
+
+            
+            <div class="infos">
+            <h2>${meals[i].strMeal}</h2>
+            <div>origin : ${meals[i].strArea}</div>
+            <button id="${meals[i].idMeal}"> En savoir plus </button>
+            </div>
+            
+            
+        
+        `
+
+        newDiv.innerHTML= container;
+        newDiv.className= 'searchContainer';
+        newDiv.id= `boxResize-${meals[i].idMeal}`;
+        results.appendChild(newDiv);
+        const btnCard = document.getElementById('btnCard');
+        btnCard.addEventListener('click', function(e){
+        console.log(e.target);
+        var searchContainer = document.querySelector('.searchContainer');
+        searchContainer.classList.toggle('active');
+        });
+         
+    }
+};
+/*<a href="${meal.strYoutube}" target="_blank"><i class="fab fa-youtube"></i></a>*/
+/* <div>category : ${meal.strCategory}</div>*/
+searchInput.addEventListener('input', (e) => {
+    search = e.target.value
+    searchDisplay();
+});
+
+
+/*function myFunction() {
+    element = document.querySelector(".searchContainer");
+    element.classList.toggle("bigContainer");
+ }*/
+
+
+
+  
+
