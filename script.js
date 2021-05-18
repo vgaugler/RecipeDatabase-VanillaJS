97af7d8830e86433eb33e6dc3e884887c5dd11b1
Victor Gaugler <victor.gaugler@hotmail.fr>
2021-05-18 09:53:44 +0200

    ajout popup

--- script.js
+++ script.js
@@ -5,137 +5,207 @@ const banner = document.querySelector('.banner');
 const bannerBox = document.querySelector('.bannerBox');
 const searchBox = document.querySelector('.searchBox');
 const searchIcon = document.getElementById('searchIcon');
+const mealDetailsContent = document.querySelector('.meal-details-content');
+const recipeCloseBtn = document.getElementById('recipe-close-btn');
+const body = document.querySelector('body');
+
+recipeCloseBtn.addEventListener('click', () => {
+  mealDetailsContent.parentElement.parentElement.classList.remove('showRecipe');
+});
 
 let search = '';
 
-window.addEventListener("scroll", function(){
-    var header = document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY > 0);
-})
+window.addEventListener('scroll', function () {
+  var header = document.querySelector('header');
+  header.classList.toggle('sticky', window.scrollY > 0);
+});
 
-
-function toggleMenu(){
-var menuToggle = document.querySelector('.toggle');
-var menu = document.querySelector('.menu');
-menuToggle.classList.toggle('active');
-menu.classList.toggle('active');
-
+function toggleMenu() {
+  var menuToggle = document.querySelector('.toggle');
+  var menu = document.querySelector('.menu');
+  menuToggle.classList.toggle('active');
+  menu.classList.toggle('active');
 }
 
-
-
-
-searchInput.addEventListener('focus',function(event){
-    banner.style.backgroundImage = "url('banner2.jpg')";
-    banner.style.transform = 'scale(1.1)';
-    bannerBox.style.transform = "translateX(350px)";
-    event.preventDefault();
-    event.stopPropagation();
-   
-
+searchInput.addEventListener('focus', function (event) {
+  banner.style.backgroundImage = "url('banner2.jpg')";
+  banner.style.transform = 'scale(1.1)';
+  bannerBox.style.transform = 'translateX(350px)';
+  event.preventDefault();
+  event.stopPropagation();
 });
 
-searchIcon.addEventListener('click', function(){
-    bannerBox.style.transform = "translateX(-5px)";
+searchIcon.addEventListener('click', function () {
+  bannerBox.style.transform = 'translateX(-5px)';
 });
 
-searchInput.addEventListener('focusout', function(event){
-    banner.style.backgroundImage = "url('banner.jpg')";
-    banner.style.transform =  'scale(1)';
-    
-    event.preventDefault();
-    event.stopPropagation();
-  
+searchInput.addEventListener('focusout', function (event) {
+  banner.style.backgroundImage = "url('banner.jpg')";
+  banner.style.transform = 'scale(1)';
+
+  event.preventDefault();
+  event.stopPropagation();
 });
 
 var topOffset;
 
-$(window).scroll(function() {
-	if($(this).scrollTop() < 1) {
-	 topOffset = 55;
-	} else {
-	 topOffset = 50;
+$(window).scroll(function () {
+  if ($(this).scrollTop() < 1) {
+    topOffset = 55;
+  } else {
+    topOffset = 50;
   }
 });
 
-$('body a[href^="#"]').on('click', function(event) {
-    $('.active').removeClass('active');
-    $(this).addClass('active');
+$('body a[href^="#"]').on('click', function (event) {
+  $('.active').removeClass('active');
+  $(this).addClass('active');
 
   var target = $(this.getAttribute('href'));
-  if( target.length ) {
-      event.preventDefault();
-      $('html, body').animate({
-        scrollTop: target.offset().top - topOffset
-      }, 1000);
+  if (target.length) {
+    event.preventDefault();
+    $('html, body').animate(
+      {
+        scrollTop: target.offset().top - topOffset,
+      },
+      1000
+    );
   }
 });
 
-const fetchSearch = async() => {
-    meals = await fetch(
-        `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
-        .then(res => res.json())
-        .then(res => res.meals);
-        console.log(meals);
-}
+const fetchSearch = async (url) => {
+  meals = await fetch(`https://www.themealdb.com/api/json/v1/1/${url}`)
+    .then((res) => res.json())
+    .then((res) => res.meals);
+  console.log(meals);
+};
 
 const searchDisplay = async () => {
-    await fetchSearch();
-    if (meals == null){
-        results.innerHTML = '<span class="noResult">No results</span>';
-    }
-    
-   results.innerHTML = ( 
+  await fetchSearch(search);
+  if (meals == null) {
+    results.innerHTML = '<span class="noResult">No results</span>';
+  }
 
-        meals.map(meal => (
-           
-            `
-            <div class="searchContainer" id="boxResize-${meal.idMeal}">
+  results.innerHTML = meals
+    .map(
+      (meal) =>
+        `
+            <div class="searchContainer">
                 
                 <img id="photo" src='${meal.strMealThumb}'/>
 
                 
                 <div class="infos">
                 <h2>${meal.strMeal}</h2>
-                <div>origin : ${meal.strArea}</div>
-                <p class="textRecette">"${meal.strInstructions}"</p>
-                <button id="${meal.idMeal}"> En savoir plus </button>
+                <div>origin : ${meal.strArea}</div> 
+                <button id="${meal.idMeal}" class="button-recipe">En savoir plus</button>
+                <div class="boxTextRecette" id="boxResize-${meal.idMeal}">
+                <p class="textRecette">${meal.strInstructions}</p>
+               
+                </div>
                 </div>
                   
                 
             </div>
-            ` 
-        )).join('')
-    ); 
-
-   for ( let i = 0; i<meals.length; i++){
-        const btn = document.getElementById(meals[i].idMeal);
-      
-        btn.addEventListener('click', (event) => {
-            const boxResizeSelect = document.getElementById(`boxResize-${event.target.id}`);
-            boxResizeSelect.classList.toggle('bigContainer');
-           /* const textRecette= document.getElementById('textRecette');
-            const instructionRecette = document.createElement('p');
-            textRecette.classList.toggle('active');
-            textRecette.textContent= meal.idMeal;
-             ${meal.strInstructions}*/
-        });
- }
+            `
+    )
+    .join('');
 };
-/*<a href="${meal.strYoutube}" target="_blank"><i class="fab fa-youtube"></i></a>*/
-/* <div>category : ${meal.strCategory}</div>*/
+results.addEventListener('click', getMealRecipe);
+
+function getMealRecipe(e) {
+  console.log(e.target.id);
+  e.preventDefault();
+  if (e.target.classList.contains('button-recipe')) {
+    let mealItem = e.target;
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.id}`)
+      .then((response) => response.json())
+      .then((response) => mealRecipeModal(response.meals[0]));
+  }
+}
+
+// create a modal
+function mealRecipeModal(meal) {
+  const ingredients = [];
+  // Get all ingredients from the object. Up to 20
+  for (let i = 1; i <= 20; i++) {
+    if (meal[`strIngredient${i}`]) {
+      ingredients.push(
+        `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+      );
+    }
+  }
+  console.log(ingredients);
+  console.log(meal);
+
+  let html = `
+    <div class="detailHeader">
+    <div class="boxTitleHeader">
+        <h2 class = "recipe-title">${meal.strMeal}</h2>
+        <p class = "recipe-category">${meal.strCategory}</p>
+        </div>
+        <div class = "recipe-meal-img">
+            <img src = "${meal.strMealThumb}" alt = "">
+        </div>
+    </div>
+        <h3 class= "title-ingredients">Ingredients:</h3>
+        <div class = "boxIngredient">
+				<ul class = "ingredients" >
+					${ingredients
+            .map(
+              (ingredient) =>
+                `<li class="ingredientsDetail"> ${ingredient}</li>`
+            )
+            .join('')}
+				</ul>
+        </div>
+        <div class = "recipe-instruct">
+            <h3>Instructions:</h3>
+            <p>${meal.strInstructions}</p>
+        </div>
+        
+        <div class = "recipe-link">
+            <a href = "${meal.strYoutube}" target = "_blank">Watch Video</a>
+        </div>
+    `;
+  mealDetailsContent.innerHTML = html;
+  mealDetailsContent.parentElement.parentElement.classList.add('showRecipe');
+}
+
 searchInput.addEventListener('input', (e) => {
-    search = e.target.value
-    searchDisplay();
+  search = `search.php?s=${e.target.value}`;
+
+  searchDisplay();
 });
 
+// GET RANDOM MEAL
+const randomMealDisplay = async () => {
+  await fetchSearch('random.php');
 
-/*function myFunction() {
-    element = document.querySelector(".searchContainer");
-    element.classList.toggle("bigContainer");
- }*/
+  results.innerHTML = meals.map(
+    (meal) =>
+      `
+      <div class="searchContainer">
+                
+      <img id="photo" src='${meal.strMealThumb}'/>
 
-
-
+      
+      <div class="infos">
+      <h2>${meal.strMeal}</h2>
+      <div>origin : ${meal.strArea}</div> 
+      <button id="${meal.idMeal}" class="button-recipe">En savoir plus</button>
+      <div class="boxTextRecette" id="boxResize-${meal.idMeal}">
+      <p class="textRecette">${meal.strInstructions}</p>
+     
+      </div>
+      </div>
+        
+      
+  </div>
   
+      `
+  );
+};
 
+randomMeal.addEventListener('click', randomMealDisplay);
+randomMealDisplay();
